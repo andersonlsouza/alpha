04020ae86940ffda2713052a3f0ff7a1f7c3da6b
Anderson L <113462710+andersonlsouza@users.noreply.github.com>
2023-07-05 18:16:09 -0300

    Aulas de Servidores

---
+++ Ciclo 2/Servidores/aula-10/desafio - Correção 2/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const roteador = require("./roteador.js")
+
+const porta = 3000;
+
+const app = express();
+app.use(express.json());
+app.use(roteador);
+
+app.use(express.static("./publico"));
+
+app.listen(porta, () => {
+    console.log(`Servidor funcionando na porta: ${porta}`);
+});
---
+++ Ciclo 2/Servidores/aula-10/desafio/controladores/controles.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+const path = "./dados/usuarios.json";
+
+const dados = JSON.parse(fs.readFileSync(path, "utf-8"));
+
+const listarUsuarios = function(req, res){
+    res.send(dados);
+    return dados;               
+}
+
+const apenasUmUsuario = function (req, res) {
+    const idUsuario = Number(req.params.id);
+    const dados = JSON.parse(fs.readFileSync(path, "utf-8"));
+    const usuario = dados.find((elemento) => elemento.id === idUsuario);
+    res.send(usuario);
+    return usuario;
+}
+
+const adicionarUsuario = function (req, res) {
+    const formulario = req.body;
+
+    const novoUsuario = {
+        id: uuid(),
+        nome: formulario.nome,
+        email: formulario.email,
+    }
+    dados.push(novoUsuario);
+
+    fs.writeFileSync(path, JSON.stringify(dados));
+
+    res.send(novoUsuario);
+
+    return novoUsuario;
+}
+
+const editarUsuario = function (req, res) {
+    const idUsuario = req.params.id;
+    const nome = req.body.nome;
+    const email = req.body.email;
+
+    const usuario = dados.find((elemento) => elemento.id === idUsuario);
+
+    if (nome !== undefined) {
+        usuario.nome = nome;        
+    }
+
+    if (email !== undefined) {
+        usuario.email = email;        
+    }
+
+    fs.writeFileSync(path, JSON.stringify(dados));
+
+    res.send(usuario);
+    
+    return usuario;
+}
+
+const deletarUsuario = function (req, res) {
+    const idUsuario = req.params.id;
+
+    const usuario = dados.find((elemento) => elemento.id === idUsuario);
+    const posicaoUsuario = dados.indexOf(usuario);
+
+    dados.splice(posicaoUsuario, 1);
+
+    fs.writeFileSync(path, JSON.stringify(dados));
+
+    res.send(usuario);
+
+    return usuario;
+}
+  
+module.exports = { listarUsuarios, apenasUmUsuario, adicionarUsuario, editarUsuario, deletarUsuario };
