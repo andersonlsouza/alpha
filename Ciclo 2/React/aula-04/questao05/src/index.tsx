2178319632462d5986de7919c097c9044afd85ee
Anderson L <113462710+andersonlsouza@users.noreply.github.com>
2024-07-25 22:06:15 -0300

    Remove webVitals

--- Ciclo 2/React/aula-04/questao05/src/index.tsx
+++ Ciclo 2/React/aula-04/questao05/src/index.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { LoginPage } from "./pages/login-page";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <LoginPage />
   </React.StrictMode>
 );
-
-reportWebVitals();
